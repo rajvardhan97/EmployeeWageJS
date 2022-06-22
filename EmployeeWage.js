26f8a08490d8e470f5b08e9c300bb7a7cfc3f045
rajvardhan97 <n.rajvardhan2627@gmail.com>
2022-06-22 14:51:14 +0530

    [Raj]Add: Calculate daily wage based on part time or full time work

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -8,3 +8,26 @@ else
 {
     console.log("Employee is Absent");
 }
+
+
+const PartTime = 1;
+const FullTime = 2;
+const PartTimeHours = 4;
+const FullTimeHours = 8;
+const WageperHour = 20;
+let Emphours = 0;
+let CheckEmp = Math.floor(Math.random() * 10) % 3;
+
+switch(CheckEmp)
+{
+    case PartTime:
+        Emphours = PartTimeHours;
+        break;
+    case FullTime:
+        Emphours = FullTimeHours;
+        break;
+    default:
+        Emphours = 0;
+}
+let EmpWage = Emphours * WageperHour;
+console.log("Employee Wage is: " + EmpWage);
